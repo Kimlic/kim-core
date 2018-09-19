47205f2cce2526b7b61ea35f719ffb3e638e4568
Dmytro Nasyrov <dmytro@pharosproduction.com>
2018-09-19 18:16:38 +0300

    change redis config

--- apps/core/config/prod.exs
+++ apps/core/config/prod.exs
@@ -1,8 +1,8 @@
 use Mix.Config
 
 config :core, 
-  redis_host: "stage-kimlic.redis.cache.windows.net", # System.get_env("REDIS_HOST"),
-  redis_password: "NxCFs4gNxvbga51ySs0gQokCBTve1JBXmMoJtIj4Dxo=" # System.get_env("REDIS_PASSWORD")
+  redis_host: "stage-redis.eastus.cloudapp.azure.com", # System.get_env("REDIS_HOST"),
+  redis_password: "298ac25d80f31148b377be0ae3c298d3185552ea5beaf983678a3fdfbd673d12" # System.get_env("REDIS_PASSWORD")
 
 config :core, Core.Clients.Mailer,
   region: "eu-west-1",
